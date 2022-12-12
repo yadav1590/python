a200a831136cee49dff514a258e46c62dcf457f7
yadav1590 <42057615+yadav1590@users.noreply.github.com>
2022-12-12 23:09:29 +0530

    Update lbstokg.py

--- lbstokg.py
+++ lbstokg.py
@@ -2,6 +2,6 @@ weight_lbs = input('enter your weight into lbs: ')
 # #to check which variable type is being used for lbs
 # print(type(weight_lbs))
 weight_kg =  int(weight_lbs) * float(0.45)
-# #to check which variable type is being used for lbs
+# #to check which variable type is being used for KGs
 # print(type(weight_kg))
 print('your weight is ' + str(weight_kg )   + ' KG')
